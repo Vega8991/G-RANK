8f56a872f4005f3142a42910e4abd0394e992927
Vega8991 <manuelvega011204@gmail.com>
2026-01-29 12:22:26 +0100

    functionality added into authController.js

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -17,7 +17,7 @@ function registerUser(req, res) {
     });
 
     newUser.save((err, savedUser) => {
-        if(err){
+        if (err) {
             res.status(400).json({
                 success: false,
                 message: 'Error creating user',
@@ -39,3 +39,57 @@ function registerUser(req, res) {
         });
     });
 }
+
+function loginUser(req, res) {
+    let email = req.body.email;
+    let password = req.body.password;
+
+    User.findOne({ email: email }, (err, foundUser) => {
+        if (err) {
+            res.status(500).json({
+                success: false,
+                message: 'Server error'
+            });
+            return;
+        }
+
+        let isPasswordValid = bcrypt.compareSync(password, foundUser.password);
+
+        if (!isPasswordValid) {
+            res.status(401).json({
+                success: false,
+                message: 'Invalid password'
+            });
+            return;
+        }
+
+        let secretKey = process.env.JWT_SECRET;
+        let expirationTime = process.env.JWT_EXPIRE;
+
+        let token = jwt.sign({
+            userId: foundUser._id,
+            email: foundUser.email,
+            username: foundUser.username
+        },
+            secretKey,
+            { expiresIn: expirationTime }
+        );
+
+        res.status(200).json({
+            success: true,
+            message: 'Login successful',
+            token: token,
+            user: {
+                id: foundUser._id,
+                username: foundUser.username,
+                email: foundUser.email,
+                rank: foundUser.rank,
+                mmr: foundUser.mmr,
+                winRate: foundUser.winRate,
+                winStreak: foundUser.winStreak
+            }
+        });
+    });
+}
+
+module.exports = { registerUser, loginUser };
