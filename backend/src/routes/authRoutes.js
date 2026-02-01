bc2599b34ee5d8acab1a65a70ef9b77d97b329d4
Vega8991 <manuelvega011204@gmail.com>
2026-02-01 15:09:05 +0100

    complete email verification system

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,10 +1,11 @@
 let express = require('express');
 let router = express.Router();
-let { registerUser, loginUser, getProfile } = require('../controllers/authController');
+let { registerUser, loginUser, getProfile, verifyEmail } = require('../controllers/authController');
 let { verifyToken } = require('../middlewares/authMiddleware');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/profile', verifyToken, getProfile);
+router.get('/verify-email', verifyEmail);
 
 module.exports = router;
