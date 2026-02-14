a948a96b2d29d603e41a688f7c9434fb393b768e
Vega8991 <manuelvega011204@gmail.com>
2026-02-14 17:34:47 +0100

    Folder structure added

--- backend/src/routes/matchResultRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const matchResultController = require('../controllers/matchResultController');
-const { verifyToken } = require('../middlewares/authMiddleware');
-
-router.post('/submit-replay', verifyToken, matchResultController.submitReplay);
-router.get('/tournament/:tournamentId', matchResultController.getTournamentResults);
-
-module.exports = router;
--- frontend/src/pages/Profile.jsx
+++
@@ -1,3 +0,0 @@
-export default function Profile() {
-    return <h1 className="p-6 text-white text-3x1">Profile</h1>
-}
